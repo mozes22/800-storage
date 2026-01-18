12b3233ba8fd19c34ba226585baedb5090c77004
Moussa Arbid <mozes_86@hotmail.com>
2026-01-18 16:06:50 +0200

    feat(routes): define application routes for navigation
    - Add routes for Home, Users, User Details, About, and Contact components
    - Implement wildcard route to redirect to Home for unmatched paths
    - Enhance user navigation experience within the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserDetailsComponent } from './pages/user-details/user-details.component';
+import { UsersComponent } from './pages/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home - 800 Storage'
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+    title: 'Users - 800 Storage'
+  },
+  {
+    path: 'users/:id',
+    component: UserDetailsComponent,
+    title: 'User Details - 800 Storage'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About - 800 Storage'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    title: 'Contact - 800 Storage'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
