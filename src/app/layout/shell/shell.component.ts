186f18bd79145a3405dd0820e2c9cf5683765e20
Moussa Arbid <mozes_86@hotmail.com>
2026-01-18 12:47:28 +0200

    feat(layout): add header, footer, and shell components
    - Implement header and footer components with responsive design
    - Create shell component to structure the layout with header and footer
    - Add Tailwind CSS styles for consistent styling across components
    - Include dynamic year display in footer for up-to-date copyright information

---
+++ src/app/layout/shell/shell.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-shell',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  templateUrl: './shell.component.html',
+  styleUrl: './shell.component.css'
+})
+export class ShellComponent {}
